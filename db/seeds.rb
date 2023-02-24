10f6d5976ff839088270c16def9e2e30751309f7
Amelie Berthier <amelieberthier.pro@gmail.com>
2023-02-24 11:48:52 +0100

    add dotenv-rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+serialized_movies = URI.open(url).read
+movies = JSON.parse(serialized_movies)
+data = movies['results']
+picture_url = 'https://image.tmdb.org/t/p/w500'
+
+data.each do |movie|
+  new_movie = Movie.create(title: movie["title"], overview: movie["overview"], poster_url: "#{picture_url}#{movie['poster_path']}", rating: movie["vote_average"])
+end
